fe34c083fa3923ae3b5bf7f5a59162f83f6ab12b
Christian <christianrmch@outlook.es>
2023-06-09 22:24:41 +0200

    TASCA – ALGORITMES 3 – DIVIDIR FACTORIALS

--- Tests/Algortimes/es/iesmz/ed/algoritmes/DividirFactorialTest.java
+++ Tests/Algortimes/es/iesmz/ed/algoritmes/DividirFactorialTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
-class DividirFactorialTest {
-  
+package Algortimes.es.iesmz.ed.algoritmes;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class DividirFactorialTest {
+    @Test
+    public void divisioTest() {
+        DividirFactorial dividirFactorial = new DividirFactorial(5, 3);
+        int resultado = dividirFactorial.divisio();
+        Assert.assertEquals(20, resultado);
+    }
+
+    @Test
+    public void divisioTestDos() {
+        DividirFactorial dividirFactorial = new DividirFactorial(6, 4);
+        int resultado = dividirFactorial.divisio();
+        Assert.assertEquals(30, resultado);
+    }
+
 }
--- src/Algortimes/es/iesmz/ed/algoritmes/DividirFactorial.java
+++ src/Algortimes/es/iesmz/ed/algoritmes/DividirFactorial.java
@@ -1,2 +1,68 @@
-package Algortimes.es.iesmz.ed.algoritmes;public class DividirFactorial {
+package Algortimes.es.iesmz.ed.algoritmes;
+
+/**
+ * Esta clase define un objeto que recibiendo dos números calcula
+ * la división de sus factoriales.
+ * @author 1ºDAM_Christian;
+ * @version: 09/06/2023
+ */
+public class DividirFactorial {
+    private int denominador;
+    private int numerador;
+
+    /**
+     * Constructor de la clase 'DividirFactorial'
+     * al que se le pasa dos números enteros, numerador y denominador.
+     * y que lo almacena en su atributo correspondiente.
+     * @param numerador de tipo Int
+     * @param denominador de tipo Int
+     */
+    public DividirFactorial(int numerador, int denominador) {
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    /**
+     * Método público 'get' que devuelve el denominador almacenado en la clase.
+     * @return el atributo privado 'denominador' de tipo int.
+     */
+    public int getDenominador() {
+        return denominador;
+    }
+
+    /**
+     * Método público 'set' que recibe un int 'denominador' y lo almacena en la clase.
+     * @param denominador de tipo int.
+     */
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    /**
+     * Método público 'get' que devuelve el numerador almacenado en la clase.
+     * @return el atributo privado 'numerador' de tipo int.
+     */
+    public int getNumerador() {
+        return numerador;
+    }
+
+    /**
+     * Método público 'set' que recibe un int 'numerador' y lo almacena en la clase.
+     * @param numerador de tipo int.
+     */
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    /**
+     * Método público 'divisio' que devuelve un número entero que es la división de dos números factoriales.
+     * @return divisionFactoriales de tipo int.
+     */
+    public int divisio(){
+        int divisonFactoriales = 1;
+        for (int i = this.denominador + 1; i <= this.numerador; i++) {
+            divisonFactoriales *= i;
+        }
+        return divisonFactoriales;
+    }
 }
